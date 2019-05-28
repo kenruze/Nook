420835abf0f4c8309c7c876892b2e32c829f7eec
elliott davis <kenruze@gmail.com>
2019-05-28 20:46:03 -0300

    Added a jump pad. configured the player prefabs a bit

---
+++ Nook/Assets/Scripts/JumpPad.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpPad : MonoBehaviour
+{
+    public float jumpForce = 10;
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("hit collider "+other.name);
+        var rigidBody = other.gameObject.GetComponentInParent<Rigidbody>();
+        if(rigidBody != null)
+        {
+            var velocity = rigidBody.velocity;
+            velocity.y = jumpForce;
+            rigidBody.velocity = velocity;
+        }
+    }
+}
